a14c3eb3c76bfe5e92df256d118c5907632f9c19
Cindy <cindy@cindy-Lenovo-G480.(none)>
2015-06-01 17:30:11 +0800

    added fields

--- ilo/pledge/content/pledge.py
+++ ilo/pledge/content/pledge.py
@@ -20,17 +20,90 @@ from plone.app.textfield import RichText
 from z3c.relationfield.schema import RelationList, RelationChoice
 from plone.formwidget.contenttree import ObjPathSourceBinder
 #from plone.multilingualbehavior.directives import languageindependent
+from quintagroup.z3cform.captcha import Captcha, CaptchaWidgetFactory
 from collective import dexteritytextindexer
+
+from zope.schema import ValidationError
+from Products.CMFDefault.utils import checkEmailAddress
+from Products.CMFDefault.exceptions import EmailAddressInvalid
 
 from ilo.pledge import MessageFactory as _
 
 
 # Interface class; used to define content-type schema.
 
+class InvalidEmailAddress(ValidationError):
+    "Invalid email address"
+
+
+def validateaddress(value):
+    try:
+        checkEmailAddress(value)
+    except EmailAddressInvalid:
+        raise InvalidEmailAddress(value)
+    return True
+
 class IPledge(form.Schema, IImageScaleTraversable):
     """
     Pledge Form
     """
+    first_name = schema.TextLine(
+           title=_(u"First Name"),
+           required=True,
+        )
+
+    last_name = schema.TextLine(
+           title=_(u"Last Name"),
+           required=True,
+        )
+
+    organization = schema.TextLine(
+           title=_(u"Organization"),
+           required=False,
+        )
+
+    designation = schema.TextLine(
+           title=_(u"Function"),
+           required=False,
+        )
+
+    # city = schema.TextLine(
+    #        title=_(u"City"),
+    #        required=False,
+    #     )
+
+    country = schema.TextLine(
+           title=_(u"Country"),
+           required=False,
+        )
+
+    email1 = schema.TextLine(
+           title=_(u"Email Address"),
+           constraint=validateaddress,
+        )
+
+    email2 = schema.TextLine(
+           title=_(u"Enter the same email address"),
+           constraint=validateaddress
+        )
+
+    captcha = Captcha(
+        title=_(u'Type the code'),
+        description=_(u'Type the code from the picture shown below.'))
+    
+    @invariant
+    def emailAddressValidation(self):
+        #pattern = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+            
+        if self.email1 != self.email2:
+            raise Invalid(_("Both email addresses do not match"))
+        
+        #if not bool(re.match(pattern, self.email1)):
+        #    raise Invalid(_(u"Email 1 is not a valid email address."))
+        #elif not bool(re.match(pattern, self.email2  )):
+        #    raise Invalid(_(u"Email 2 is not a valid email address."))
+
+
     pass
 
 alsoProvides(IPledge, IFormFieldProvider)
