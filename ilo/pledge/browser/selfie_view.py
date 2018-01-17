cfd1987814d6ecc1bce31922a970c816638ec3a9
glenn27 <glenn@afterfivetech.com>
2018-01-17 16:41:08 +0800

    added function to get parent text_direction value

--- ilo/pledge/browser/selfie_view.py
+++ ilo/pledge/browser/selfie_view.py
@@ -9,4 +9,12 @@ class Index(dexterity.DisplayForm):
     grok.require('zope2.View')
     grok.template('selfie_view')
     grok.name('view')
+    
+    def text_direction_value(self):
+        parent = self.context.aq_parent
+        value = 'ltr'
+        if hasattr(parent, 'text_direction'):
+            if parent.text_direction:
+                value = parent.text_direction
+        return value
 
